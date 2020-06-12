5cb66d90760a6b6a5f88494b334720b158d4d608
Juana Villalpando <juana.v14@gmail.com>
2020-06-12 08:42:49 -0700

    names complete (maybe stretch here)

--- names/names.py
+++ names/names.py
@@ -9,20 +9,33 @@ f.close()
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
+#convert names_2 to set for fster look up.
+#A set is a collection which is unordered and unindexed. In Python sets are written with curly brackets.
+names_2 = set(names_2)
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if name_1 in names_2:
+        duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+
+#Before optimiation: 9.4sec
+#After optimiation: 1.91 sec
+
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+#After 2nd optimiation (with set): 0.0094 seconds
